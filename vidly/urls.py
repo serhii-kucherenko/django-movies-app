c2cab9e1f8509410082e074bbe378a77ff0edebe
Serhii Kucherenko <serhii.kucherenko@windmill.ch>
2022-11-13 16:33:46 +0200

    Add home page

--- vidly/urls.py
+++ vidly/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from api.models import MovieResource
+from . import views
 
 movie_resource = MovieResource()
 
 urlpatterns = [
+    path('', views.home),
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('api/', include(movie_resource.urls)),
